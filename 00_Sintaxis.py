7bdb33884a07cd4f4a811012217d16c298b218da
santiagotuc <santiagoemmanuelmolina@gmail.com>
2024-05-23 16:44:10 -0300

    Add files via upload

---
+++ 00_Sintaxis.py
@@ -0,0 +1,58 @@
+'''
+  ¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
+  - Recuerda que todas las instrucciones de participación están en el
+    repositorio de GitHub.
+ 
+  Lo primero... ¿Ya has elegido un lenguaje?
+  - No todos son iguales, pero sus fundamentos suelen ser comunes.
+  - Este primer reto te servirá para familiarizarte con la forma de participar
+    enviando tus propias soluciones.
+ 
+  EJERCICIO:
+  1- Crea un comentario en el código y coloca la URL del sitio web oficial del
+    lenguaje de programación que has seleccionado.
+  2- Representa las diferentes sintaxis que existen de crear comentarios
+    en el lenguaje (en una línea, varias...).
+  3- Crea una variable (y una constante si el lenguaje lo soporta).
+  4- Crea variables representando todos los tipos de datos primitivos
+    del lenguaje (cadenas de texto, enteros, booleanos...).
+  5- Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ 
+  ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+  debemos comenzar por el principio.
+'''
+#1
+# https://www.python.org/
+
+#2
+# Comentario en una linea
+
+'''
+Comentario en varias lineas 2
+'''
+
+"""
+Comentario en varias lineas 3"""
+
+#3
+mi_variable = 'variable1'
+
+MI_CONSTANTE = 'miconstante' #Por convencion
+
+#4 VARIABLES DATOS PRIMITIVOS
+
+var_string = 'Hola'
+var_int = 1
+var_float = 1.5
+var_bool = True
+var_bool = False
+
+#5 
+
+print('Hola Python')
+print(type(var_string))
+print(type(var_int))
+print(type(var_float))
+print(type(var_bool))
+
+
